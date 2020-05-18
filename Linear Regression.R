c6115d43237120348b21f42ee434a8deaab25a3b
Vivek Mohan <63765263+vivekmohan79@users.noreply.github.com>
2020-05-18 09:26:17 +0530

    My First File Upload
    My First File Upload

---
+++ Linear Regression.R
@@ -0,0 +1,20 @@
+setwd("C:/Users/vivek_mohan/Desktop/Training/Documentation/R/")
+
+customer_churn <- read.csv("customer_churn.csv")
+
+library(caTools)
+
+library(ROCR)
+
+install.packages("caTools")
+install.packages("ROCR")
+
+customer_churn
+
+install.packages("dplyr")
+
+library("dplyr")
+
+install.packages("stringr")
+library(stringr)
+
